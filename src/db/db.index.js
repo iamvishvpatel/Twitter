c7af60a98840f7e88a57059e6f88331961a14cec
Vishv Patel Developer <125183449+iamvishvpatel@users.noreply.github.com>
2025-07-01 19:02:02 +0530

    Update db.index.js

--- src/db/db.index.js
+++ src/db/db.index.js
@@ -6,6 +6,7 @@ import mysql from 'mysql2/promise'
         const connection =  mysql.createConnection({
           host: process.env.DB_HOST,
           user: process.env.DB_USER,
+            port: 3307,
           password: process.env.DB_PASSWORD,
           database: process.env.DB_NAME,
         });
